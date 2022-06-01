168f6194a48761a80959c46850cbe263560bf28a
Mayuran <mayuranswee23@gmail.com>
2022-06-01 00:46:48 -0400

    add read, delete and create queries for sql queries

--- server.js
+++ server.js
@@ -20,6 +20,39 @@ const db = mysql.createConnection(
     console.log('Connected to the Election Database')
 )
 
+//return all data in the candidates table
+// db.query(`SELECT * FROM candidates`, (err, rows)=> {
+//     console.log(rows)
+// });
+
+//GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id=1`, (err, row) => {
+    if (err){
+        console.log(err);
+    }
+    console.log(row);
+})
+
+//delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result)=>
+// {
+//     if (err){
+//         console.log(err)
+//     }
+//     console.log(result);
+// });
+
+//create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_related)
+VALUES(?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result)=> {
+    if (err){
+        console.log(err);
+    }
+    console.log(result)
+});
 
 //default response for other requests (not found)
 app.use((req, res)=> {
