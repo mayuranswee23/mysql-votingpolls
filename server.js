d4b7c4dd575ecab0a61fa689a7623b7a7dda6701
Mayuran <mayuranswee23@gmail.com>
2022-06-01 00:11:15 -0400

    connect server to database

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require ("mysql2");
 
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to a database
+const db = mysql.createConnection(
+    {
+        host: 'localhost', 
+        //mySQL username, 
+        user: 'root', 
+        //mysql password
+        password: 'CoderBootcamp1!', 
+        database: 'election'
+    }, 
+    console.log('Connected to the Election Database')
+)
 
 
 //default response for other requests (not found)
@@ -15,5 +29,5 @@ app.use((req, res)=> {
 
 //start express server on PORT 3001 
 app.listen(PORT, ()=> {
-    console.log(`Server is running on Port ${PORT}`)
+    console.log(`Server is running on PORT ${PORT}`)
 });
